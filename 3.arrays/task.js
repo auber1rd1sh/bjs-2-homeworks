f3ebb0941ed47948f69975f9f148c12908243994
auber1rd1sh <kristinina333@gmail.com>
2024-09-21 23:09:57 +0300

    Реализованы тесты из третьего задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every(i => arr1[i] === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).reduce((sum, user) => sum + user.age, 0);
+	let count = users.filter(user => user.gender === gender).length;
+    
+	if (count === 0) {
+		return 0;
+	}
+
+	return result / count;
 }
